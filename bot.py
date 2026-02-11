08b81067ee6b79c29d31ab63eeb28ccb410b2335
madhuthakur-tech <madhuthakur00488@gmail.com>
2026-02-11 12:34:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,28 @@
+import os
 from telegram import Update
-from telegram.ext import ContextTypes, MessageHandler, filters
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+
+TOKEN = os.getenv("BOT_TOKEN")  # Railway/Render me env variable me token daalo
 
 async def get_video_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.video:
-        await update.message.reply_text(
-            f"VIDEO FILE ID:\n{update.message.video.file_id}"
-        )
+    video = update.message.video
 
-app.add_handler(MessageHandler(filters.VIDEO, get_video_id))
+    file_id = video.file_id
+    file_unique_id = video.file_unique_id
+
+    await update.message.reply_text(
+        f"🎥 Video File ID:\n\n{file_id}\n\n"
+        f"🔹 Unique ID:\n{file_unique_id}"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(MessageHandler(filters.VIDEO, get_video_id))
+
+    print("Bot running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
 
