1c0d85037507697ef22e2e5180b038ce9f2b5df8
madhuthakur-tech <madhuthakur00488@gmail.com>
2026-02-11 12:29:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,73 +1,8 @@
-import logging
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+from telegram.ext import MessageHandler, filters
 
-import os
+async def get_video_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    video = update.message.video
+    if video:
+        await update.message.reply_text(f"VIDEO FILE ID:\n{video.file_id}")
 
-TOKEN = os.getenv("BOT_TOKEN")
-
-
-VIDEO_FILE_ID = "BAACAgUAAxkBAAMOaYwnfQORM8KWDTZBe_IWjnf6TG8AAn4mAAKqQWFUulkQx94d5HA6BA"
-
-CHANNEL_LINKS = [
-    "https://t.me/+_YmoMrDZ0oliMTll",
-    "https://t.me/+-s8gGlM-BcY1NDll",
-    "https://t.me/+az-lgmrUAnU1MzQ1",
-    "https://t.me/+Ltw6NlDYtaQ5OWE1"
-]
-
-logging.basicConfig(level=logging.INFO)
-
-
-# START
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("📢 Join Channel", url=link)] for link in CHANNEL_LINKS]
-
-    keyboard.append([InlineKeyboardButton("✅ Check Status", callback_data="check")])
-
-    await update.message.reply_text(
-        "⚠️ Sabhi channels join karo.\n\nPhir 'Check Status' dabao.",
-        reply_markup=InlineKeyboardMarkup(keyboard)
-    )
-
-
-# FIRST CHECK (NO VIDEO)
-async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    keyboard = [[InlineKeyboardButton("📢 Join Channel", url=link)] for link in CHANNEL_LINKS]
-
-    keyboard.append([InlineKeyboardButton("✅ Ab Bhej Di", callback_data="confirm")])
-
-    await query.message.reply_text(
-        "❌ Request confirm nahi hui.\n\n👉 Fir se request bhejo.\n\nBhej diya ho to niche button dabao.",
-        reply_markup=InlineKeyboardMarkup(keyboard)
-    )
-
-
-# SECOND CONFIRM (SEND VIDEO)
-async def confirm(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    await query.message.reply_video(
-        video=VIDEO_FILE_ID,
-        caption="🔥 Ye lo tumhara video 😎"
-    )
-
-
-# MAIN
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CallbackQueryHandler(check, pattern="check"))
-    app.add_handler(CallbackQueryHandler(confirm, pattern="confirm"))
-
-    print("Bot running...")
-    app.run_polling()
-
-
-if __name__ == "__main__":
-    main()
+app.add_handler(MessageHandler(filters.VIDEO, get_video_id))
